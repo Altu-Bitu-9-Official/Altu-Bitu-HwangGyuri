db3f7ef2a012f1ee026a3be5a02c7e80a1589ac2
gyuuuuri <gyuri113@ewha.ac.kr>
2025-10-06 23:41:19 +0900

    [04_브루트포스] 10월 6일

---
+++ 04_브루트포스/1476.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+// 범위 설정 (1 ≤ E ≤ 15, 1 ≤ S ≤ 28, 1 ≤ M ≤ 19)
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    //입력 (지구, 태양, 달)
+    int e, s, m;
+    cin >> e >> s >> m;
+
+    //연산
+    //출력
+
+}
