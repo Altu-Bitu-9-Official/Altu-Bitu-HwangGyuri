33fdf985af2ddcb7ed9428bb2006c7f052b72d19
gyuuuuri <gyuri113@ewha.ac.kr>
2025-10-07 22:40:02 +0900

    [04_브루트포스] 10월 7일

--- 04_브루트포스/1476.cpp
+++ 04_브루트포스/1476.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
-// 범위 설정 (1 ≤ E ≤ 15, 1 ≤ S ≤ 28, 1 ≤ M ≤ 19)
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -13,7 +11,23 @@ int main(){
     int e, s, m;
     cin >> e >> s >> m;
 
-    //연산
-    //출력
-
+    // 연산 
+    for (int year = 1; year <= 7980; year++){
+        //입력값과 비교할 변수 각각 세팅 
+        int E = year % 15;
+        if (E == 0){
+            E = 15; 
+        }
+        int S = year % 28;
+        if (S == 0){
+            S = 28; 
+        }
+        int M = year % 19; 
+        if (M == 0){
+            M = 19; 
+        }
+        if (E==e && S==s && M==m){
+            cout << year << '\n'; 
+        }
+    }
 }
